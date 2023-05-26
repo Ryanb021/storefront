12f32967fce0781612cd95ff5a56d68f07c8a3c7
Ryan Bagan <darcknight1980@gmail.com>
2023-05-25 22:19:13 -0700

    rtk

--- src/store/categories/index.jsx
+++ src/store/categories/index.jsx
@@ -1,26 +1,49 @@
-let initialState = {
-  categories: [
-    { name: 'electronics', displayName: 'Electronics', description: 'Electronics on Sale!' },
-    { name: 'food', displayName: 'Food', description: 'Food gives you energy.'},
-    { name: 'clothing', displayName: 'Clothing', description: 'Clothing that will make you look good.' },
-  ],
-  activeCategory: ''
-};
+import { createSlice } from '@reduxjs/toolkit';
 
-const categoryReducer = (state=initialState, action) => {
-  const { type, payload } = action;
-
-  switch(type){
-    case 'SET':
-      return {
-        ...state,
-        activeCategory: payload,
-      };
-    case 'RESET':
-      return initialState;
-    default: 
-      return state;
+const categorySlice = createSlice({
+  name: 'categories',
+  initialState: {
+    categories: [],
+    activeCategory: '',
+  },
+  reducers: {
+    setCategory: (state, action) => {
+      state.activeCategory = action.payload.name;
+    },
+    initialSetCategories: (state, action) => {
+      state.categories = action.payload;
+    }
   }
-};
 
-export default categoryReducer;
+});
+
+export const { setCategory, initialSetCategories } = categorySlice.actions;
+export default categorySlice.reducer;
+
+
+// let initialState = {
+//   categories: [
+//     { name: 'electronics', displayName: 'Electronics', description: 'Electronics on Sale!' },
+//     { name: 'food', displayName: 'Food', description: 'Food gives you energy.'},
+//     { name: 'clothing', displayName: 'Clothing', description: 'Clothing that will make you look good.' },
+//   ],
+//   activeCategory: ''
+// };
+
+// const categoryReducer = (state=initialState, action) => {
+//   const { type, payload } = action;
+
+//   switch(type){
+//     case 'SET':
+//       return {
+//         ...state,
+//         activeCategory: payload,
+//       };
+//     case 'RESET':
+//       return initialState;
+//     default: 
+//       return state;
+//   }
+// };
+
+// export default categoryReducer;
--- src/store/products/index.jsx
+++ src/store/products/index.jsx
@@ -1,38 +1,67 @@
-const initialState = [
-  { name: 'TV', category: 'electronics', price: 800.00, inStock: 10 },
-  { name: 'Radio', category: 'electronics', price: 150.00, inStock: 30 },
-  { name: 'Shirt', category: 'clothing', price: 15.00, inStock: 50 },
-  { name: 'Socks', category: 'clothing', price: 10.00, inStock: 40 },
-  { name: 'Apples', category: 'food', price: 1.99, inStock: 300 },
-  { name: 'Eggs', category: 'food', price: 4.99, inStock: 48 },
-  { name: 'Bread', category: 'food', price: 4.50, inStock: 60 },
-];
+import { createSlice } from '@reduxjs/toolkit';
 
-let tempState = [...initialState];
+let tempState = [];
 
-const productsReducer = (state = initialState, action) => {
-  const { type, payload } = action;
-
-  switch (type) {
-    case 'SET':
-      return initialState.filter(product => product.category === payload.name)
-    case 'ADD-PRODUCT':
-
-      let activeCategory = payload.category;
-      console.warn(activeCategory)
-
-      tempState = tempState.map(product => product.name === payload.name ? {...product, inStock: product.inStock - 1} : product)
-
-      let results = tempState.filter(product => product.category === activeCategory);
-      console.log('is inventory correct for all categories?', tempState);
-      console.log(results);
-     
-      return results
-    case 'RESET':
-      return initialState;
-    default:
-      return state;
+const productSlice = createSlice({
+  name: 'products',
+  initialState: [],
+  reducers: {
+    setProducts: (state, action) => {
+      return tempState.filter(e => e.category === action.payload.name);
+    },
+    updateProduct: (state, action) => {
+      return tempState.map(product => {
+        if (product.name === action.payload.name) {
+          return action.payload;
+        }
+        return product;
+      })
+    },
+    initialSetProducts: (state, action) => {
+      tempState = [...action.payload];
+      return action.payload;
+    }
   }
-};
+})
 
-export default productsReducer;
+export const { setProducts, updateProduct, initialSetProducts } = productSlice.actions;
+export default productSlice.reducer;
+
+// const initialState = [
+//   { name: 'TV', category: 'electronics', price: 800.00, inStock: 10 },
+//   { name: 'Radio', category: 'electronics', price: 150.00, inStock: 30 },
+//   { name: 'Shirt', category: 'clothing', price: 15.00, inStock: 50 },
+//   { name: 'Socks', category: 'clothing', price: 10.00, inStock: 40 },
+//   { name: 'Apples', category: 'food', price: 1.99, inStock: 300 },
+//   { name: 'Eggs', category: 'food', price: 4.99, inStock: 48 },
+//   { name: 'Bread', category: 'food', price: 4.50, inStock: 60 },
+// ];
+
+// let tempState = [...initialState];
+
+// const productsReducer = (state = initialState, action) => {
+//   const { type, payload } = action;
+
+//   switch (type) {
+//     case 'SET':
+//       return initialState.filter(product => product.category === payload.name)
+//     case 'ADD-PRODUCT':
+
+//       let activeCategory = payload.category;
+//       console.warn(activeCategory)
+
+//       tempState = tempState.map(product => product.name === payload.name ? {...product, inStock: product.inStock - 1} : product)
+
+//       let results = tempState.filter(product => product.category === activeCategory);
+//       console.log('is inventory correct for all categories?', tempState);
+//       console.log(results);
+     
+//       return results
+//     case 'RESET':
+//       return initialState;
+//     default:
+//       return state;
+//   }
+// };
+
+// export default productsReducer;
